d77a0174be40c8d6cf3fe023e9d10ed67fb6ef67
ArtSwing <hbyoo94@naver.com>
2019-04-08 01:59:33 +0900

    0408ver

--- Project/View/Main.java
+++ Project/View/Main.java
@@ -19,6 +19,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.table.*;
+
+import DAO.practice;
+
 import javax.swing.GroupLayout.Alignment;
 
 
@@ -440,7 +443,15 @@ public class Main extends JFrame {
 			francepanel.setVisible(false);
 		
 	}
-
+	 public void start() {
+         Main panel = new Main();
+         JFrame st = new JFrame();
+         
+         st.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         st.add(panel);
+         st.setSize(540,400);
+         st.setVisible(true);
+     }
 	
 
 	private static void addPopup(Component component, final JPopupMenu popup) {
